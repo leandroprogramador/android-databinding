5498dbc26da7a7c1db0dae89ac2c45ecc9b7a25c
Leandro <lean.ib@hotmail.com>
2021-07-14 12:12:48 -0300

    Adicionando o projeto

---
+++ app/src/main/java/br/com/alura/ceep/ui/recyclerview/adapter/ListaNotasAdapter.kt
@@ -0,0 +1,95 @@
+package br.com.alura.ceep.ui.recyclerview.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LifecycleRegistry
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import br.com.alura.ceep.databinding.ItemNotaBinding
+import br.com.alura.ceep.model.Nota
+import br.com.alura.ceep.ui.databinding.NotaData
+
+class ListaNotasAdapter(
+    private val context: Context,
+    var onItemClickListener: (notaSelecionada: Nota) -> Unit = {}
+) : ListAdapter<Nota, ListaNotasAdapter.ViewHolder>(DiffCallback) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val inflater = LayoutInflater.from(context)
+        val viewDataBinding =
+            ItemNotaBinding.inflate(inflater, parent, false)
+        return ViewHolder(viewDataBinding).also {
+            viewDataBinding.lifecycleOwner = it
+        }
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        getItem(position)?.let { nota ->
+            holder.vincula(nota)
+        }
+    }
+
+    override fun onViewAttachedToWindow(holder: ViewHolder) {
+        super.onViewAttachedToWindow(holder)
+        holder.marcaComoAtivo()
+    }
+
+    override fun onViewDetachedFromWindow(holder: ViewHolder) {
+        super.onViewDetachedFromWindow(holder)
+        holder.marcaComoDesativado()
+    }
+
+    inner class ViewHolder(private val viewDataBinding: ItemNotaBinding) : RecyclerView.ViewHolder(viewDataBinding.root), View.OnClickListener, LifecycleOwner {
+
+        private lateinit var nota: Nota
+        private val registry = LifecycleRegistry(this)
+
+
+
+        init {
+            viewDataBinding.clicaNaNota = this
+            registry.markState(Lifecycle.State.INITIALIZED)
+
+        }
+
+        fun marcaComoAtivo(){
+            registry.markState(Lifecycle.State.STARTED)
+        }
+
+        fun marcaComoDesativado(){
+            registry.markState(Lifecycle.State.DESTROYED)
+        }
+
+        fun vincula(nota: Nota) {
+            this.nota = nota
+            viewDataBinding.nota = NotaData(nota)
+
+        }
+
+        override fun onClick(view: View?) {
+            if (::nota.isInitialized) {
+                onItemClickListener(nota)
+            }
+        }
+
+        override fun getLifecycle(): Lifecycle = registry
+
+    }
+
+}
+
+object DiffCallback : DiffUtil.ItemCallback<Nota>() {
+    override fun areItemsTheSame(
+        oldItem: Nota,
+        newItem: Nota
+    ) = oldItem.id == newItem.id
+
+    override fun areContentsTheSame(oldItem: Nota, newItem: Nota) = oldItem == newItem
+
+}
+
